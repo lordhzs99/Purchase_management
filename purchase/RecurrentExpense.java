1d440fd28b547d6746556636e086b759af6a6be3
Carlos Manuel Alvarez Lopez <carlosmal1502@gmail.com>
2023-12-04 17:14:15 -0600

    Expense

--- purchase/RecurrentExpense.java
+++ purchase/RecurrentExpense.java
@@ -2,12 +2,26 @@ package purchase;
 
 public class RecurrentExpense extends Expense{
 
-    @Override
-    float getPercentage() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getPercentage'");
+    private int period;
+
+    private RecurrentExpense(double amount, String concept, String date, int period){
+        this.amount = amount;
+        this.concept = concept;
+        this.date = date;
+        this.period = period;
     }
 
-    
-    
+    public static RecurrentExpense newRecurrentExpense(double amount, String concept, String date, int period){
+        if(Expense.datePattern.matcher(date).matches()){
+            return new RecurrentExpense(amount, concept, date, period);   
+        }else{
+            return null;
+        }
+    }
+
+    @Override
+    float getPercentage() {
+        return (float)(amount*(30/period)*100/Expense.total);
+    }
+        
 }
