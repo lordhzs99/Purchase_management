0e0ae2f2aef5da15e9617a9145728ca069f2eb5c
TomVolta <112505169+TomVolta@users.noreply.github.com>
2023-12-04 18:18:19 -0600

    Update Reminder.java

--- users/Reminder.java
+++ users/Reminder.java
@@ -1,1 +1,25 @@
+class Reminder {
+    private List<RecurrentExpense> recurrentExpenses;
 
+    public Reminder() {
+        this.recurrentExpenses = new ArrayList<>();
+    }
+
+    public void addExpense(RecurrentExpense expense) {
+        recurrentExpenses.add(expense);
+    }
+
+    public void remindUser() {
+        LocalDate today = LocalDate.now();
+
+        for (RecurrentExpense expense : recurrentExpenses) {
+            LocalDate nextPaymentDate = today.plus(expense.getRecurrenceDays(), ChronoUnit.DAYS);
+
+            // Recordar al usuario un día antes del pago
+            if (nextPaymentDate.minus(1, ChronoUnit.DAYS).isEqual(today)) {
+                System.out.println("Recordatorio: Pago próximo para " + expense.getName() +
+                        " de " + expense.getAmount() + " programado para mañana.");
+            }
+        }
+    }
+}
